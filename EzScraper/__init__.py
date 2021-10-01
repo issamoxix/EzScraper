bd9d25b57e4905be26d687f41ed0e49d6ccfb994
issamoxix <issamhaidaoui@gmail.com>
2021-10-01 12:18:47 +0100

    initial

--- EzScraper/__init__.py
+++
@@ -1,1 +0,0 @@
-from table_to_csv import table_to_csv
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '1.0.0'
 DESCRIPTION = 'Scraping HTML Tables from websites to csv file'
 LONG_DESCRIPTION = 'A package that allows to scrape html table from websites easly'
 
